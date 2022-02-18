a523d7a06d8d33a313d0731a5353eabfe96479d3
Nicolas Janke Gomes <njg0@hotmail.com>
2022-02-18 19:16:47 -0300

    Criada Classe Carro

---
+++ oo/carro.py
@@ -0,0 +1,100 @@
+""" Você deve criar uma classe carro que vai possuir
+dois atributos compostos por outras duas classes
+1) Motor
+2) Direção
+
+O Motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dado: velocidade
+2) Método acelerar, que deverá incrementar a velocidade em uma unidade
+3) Método frear, que deverá decrementar a velocidade em duas unidades
+
+A direção terá a reponsabilidade de controlar a direção. Ela oferece
+os seguintes atributos:
+1) Valor da direção com os valores possíveis: Norte, Sul, Leste, Oeste
+2) Método girar a direita
+3) Método girar a esquerda
+  N
+O   L
+  S
+    Exemplo:
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+    >>> motor.acelerar()
+    >>> motor.velocidade
+    1
+    >>> motor.acelerar()
+    >>> motor.velocidade()
+    2
+    >>> motor.acelerar
+    >>> motor.velocidade
+    3
+    >>> motor.frear()
+    >>> motor.velocidade()
+    1
+    >>> motor.frear()
+    >>> motor.velocidade()
+    0
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    "Norte"
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    "Leste"
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    "Sul"
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    "Oeste"
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    "Norte" #mod direcao = 0
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    "Oeste" #mod direcao = 1
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    "Sul" #mod direcao = 2
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    "Leste" #mod direcao = 3
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    "Norte"
+    >>> carro = Carro(direcao,motor)
+"""
+from pessoa import Pessoa
+
+
+class Carro:
+    pneus = 5
+
+    def __init__(self, *passageiros, modelo=None, motor=0, direcao=0):
+        self.modelo = modelo
+        self.motor = motor
+        self.direcao = direcao
+        self.passageiros = list(Pessoa)
+
+    def testar_motor(self):
+        return self.motor
+
+    def acelerar(self):
+        self.motor = self.motor + 1
+
+    def desacelerar(self):
+        if self.motor <= 1:
+            self.motor = 0
+        else:
+            self.motor = self.motor - 2
+
+    def check_direcao(self):
+        return self.direcao
+
+    def virar_direita(self):
+        self.direcao = self.direcao + 1
+
+    def virar_esquerda(self):
+        self.direcao = self.direcao - 1
+
